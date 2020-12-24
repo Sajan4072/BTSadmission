8a3015ce86765c9e7436d20df2199a0bc8c551d9
Sajan4072 <rai4072@gmail.com>
2020-12-24 13:57:17 +0545

    engineering fixed

--- engineering/results/fetch_result.php
+++ engineering/results/fetch_result.php
@@ -1,22 +1,32 @@
 <?php 
 
-  include("connection.php");
+  include("../include/connection.php");
 $semester=$_GET['semester'];
 $code=$_GET['uniquecode'];
 $term=$_GET['term'];
 
 
 
-
+$fetch_result=array();
    $sql="select *from enginneringresult where uniquecode='$code' AND semester='$semester'  AND term='$term' " ;
-    
-   $result=mysqli_query($db,$sql);
-   $fetch_result=array();
- $res=mysqli_fetch_array($result);
-   while($row=mysqli_fetch_array($result))
-  {
-      $fetch_result[]=$row;
+   
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+   $i=0; 
+  while($row = $result->fetch_assoc()) {
+  	  $fetch_result[$i]['uniquecode']=$row['uniquecode'];
+  	  $fetch_result[$i]['semester']=$row['semester'];
+  	  $fetch_result[$i]['id']=$row['id'];
+  	  $fetch_result[$i]['roll']=$row['roll'];
+  	  $fetch_result[$i]['term']=$row['term'];
+  	  $fetch_result[$i]['subject']=$row['subject'];
+  	  $fetch_result[$i]['marks']=$row['marks'];
+        $i++;
+     
   }
+} 
+  
 
  echo json_encode($fetch_result);
 
